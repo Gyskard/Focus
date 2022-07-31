0a6e7f1c8db46c62912f04290171b4e489aed5f7
Thomas M <thomas@marguerit.at>
2022-07-31 10:14:49 +0200

    updated code_quality script and added code_quality_fix for auto-fix, solve code quality issue for unit test

--- src/back/api/tests/unit/db/connection.ts
+++ src/back/api/tests/unit/db/connection.ts
@@ -1,14 +1,16 @@
-import assert from 'assert'
+import assert from 'assert';
 
 import sequelize from '../../../modules/db/connection';
 
 describe('Database connection', () => {
-      it('should return true when it is connected to the database', async() => {
-        let isConnected: boolean = false
-        try {
-            await sequelize.authenticate()
-            isConnected = true
-        } catch (error) {}
-        assert.equal(isConnected, true)
-      });
+  it('should return true when it is connected to the database', async () => {
+    let isConnected: boolean = false;
+    try {
+      await sequelize.authenticate();
+      isConnected = true;
+    } catch (error) {
+      console.error(error); // eslint-disable-line no-console
+    }
+    assert.equal(isConnected, true);
   });
+});
